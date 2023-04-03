5c016cda1b6fdbb96a0194a3563331044d97ed12
agustinso <agustinmoralesdaniel@gmail.com>
2023-04-03 11:24:25 -0300

    added forecast to SMN provider. Needs testing for api error or missing data

--- src/providers/smn.rs
+++ src/providers/smn.rs
@@ -4,46 +4,72 @@ use reqwest::{header::{USER_AGENT, HeaderMap, HOST, ACCEPT, ACCEPT_ENCODING, ACC
 use crate::providers::common::{WeatherData, Wind};
 use crate::providers::common::WeatherType;
 
+
 #[derive(Deserialize, Debug)]
-struct SMNWeather{
+struct NowWeather{
     pub(crate) description: String,
     pub(crate) id: usize
 }
 
 #[derive(Deserialize, Debug)]
-struct SMNWind{
+struct NowWind{
     pub(crate) direction: String,
     pub(crate) deg: f32,
     pub(crate) speed: f32,
 }
 
+
 #[derive(Deserialize, Debug)]
-struct SMNParser {
+struct NowParser {
     pub(crate) date: String,
     pub(crate) humidity: f32,
     pub(crate) pressure: f32,
     pub(crate) temperature: f32,
-    pub(crate) weather: SMNWeather,
-    pub(crate) wind: SMNWind,
+    pub(crate) weather: NowWeather,
+    pub(crate) wind: NowWind,
+}
+
+#[derive(Deserialize, Debug)]
+struct ForecastWind{
+    pub(crate) direction: String,
+    pub(crate) deg: f32,
+    pub(crate) speed_range: Vec<f32>,
+}
+
+#[derive(Deserialize, Debug)]
+struct ForecastLapse {
+    humidity: Option<f32>,
+    rain_prob_range: Option<Vec<u8>>,
+    temperature: f32,
+    weather: NowWeather,
+    wind: ForecastWind,
+}
+
+#[derive(Deserialize, Debug)]
+struct ForecastEntry {
+    date: String,
+    temp_min: f32,
+    temp_max: f32,
+    humidity_min: f32,
+    humidity_max: f32,
+    early_morning: Option<ForecastLapse>,
+    morning: Option<ForecastLapse>,
+    afternoon: Option<ForecastLapse>,
+    night: Option<ForecastLapse>,
+}
+
+#[derive(Deserialize, Debug)]
+struct ForecastParser {
+    pub(crate) updated: String,
+    forecast: Vec<Option<ForecastEntry>>,
 }
 
 
-pub async fn now() -> Result<WeatherData, Box<dyn std::error::Error>> {
-    let mut headers: HeaderMap = HeaderMap::new();
-    headers.insert(HOST, "ws1.smn.gob.ar".parse().unwrap());
-    headers.insert(USER_AGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/111.0".parse().unwrap());
-    headers.insert(ACCEPT, "application/json".parse().unwrap());
-    headers.insert(ACCEPT_LANGUAGE, "es-AR".parse().unwrap());
-    headers.insert(ACCEPT_ENCODING, "GZIP, DEFLATE, br".parse().unwrap());
-    headers.insert(ORIGIN, "https://www.smn.gob.ar".parse().unwrap());
-    headers.insert(CONNECTION, "keep-alive".parse().unwrap());
-    headers.insert(REFERER, "https://www.smn.gob.ar/".parse().unwrap());
-    headers.insert(HeaderName::from_static("sec-fetch-dest"), "empty".parse().unwrap());
-    headers.insert(HeaderName::from_static("sec-fetch-mode"), "cors".parse().unwrap());
-    headers.insert(HeaderName::from_static("sec-fetch-site"), "same-site".parse().unwrap());
 
+
+async fn get_token() -> Result<String, Box<dyn std::error::Error>> {
     let token_response = Client::new().get("https://www.smn.gob.ar/")
-        .header(USER_AGENT, headers[USER_AGENT].clone())
+        .header(USER_AGENT, "notabot")
         .send()
         .await?
         .text()
@@ -63,21 +89,45 @@ pub async fn now() -> Result<WeatherData, Box<dyn std::error::Error>> {
         eprintln!("ERROR: Parsing start of token");
     }
 
+    return Ok(token);
+}
+
+
+fn make_headers(token: String) -> HeaderMap {
+    let mut headers: HeaderMap = HeaderMap::new();
+    headers.insert(HOST, "ws1.smn.gob.ar".parse().unwrap());
+    headers.insert(USER_AGENT, "notabot".parse().unwrap());
+    headers.insert(ACCEPT, "application/json".parse().unwrap());
+    headers.insert(ACCEPT_LANGUAGE, "es-AR".parse().unwrap());
+    headers.insert(ACCEPT_ENCODING, "GZIP, DEFLATE, br".parse().unwrap());
+    headers.insert(ORIGIN, "https://www.smn.gob.ar".parse().unwrap());
+    headers.insert(CONNECTION, "keep-alive".parse().unwrap());
+    headers.insert(REFERER, "https://www.smn.gob.ar/".parse().unwrap());
+    headers.insert(HeaderName::from_static("sec-fetch-dest"), "empty".parse().unwrap());
+    headers.insert(HeaderName::from_static("sec-fetch-mode"), "cors".parse().unwrap());
+    headers.insert(HeaderName::from_static("sec-fetch-site"), "same-site".parse().unwrap());
     headers.append(AUTHORIZATION, token.parse().unwrap());
 
+    return headers;
+}
+
+
+
+pub async fn now() -> Result<WeatherData, Box<dyn std::error::Error>> {
+    
+    let headers = make_headers(get_token().await?);
 
     let client = reqwest::Client::new();
 
     let now_resp = client
         .get("https://ws1.smn.gob.ar/v1/weather/location/7706")
-        .header(USER_AGENT, "tupapichulo")
         .headers(headers)
         .send()
         .await?
         .text()
         .await?;
 
-    let now: SMNParser = serde_json::from_str(now_resp.as_str()).unwrap();
+    let now: NowParser = serde_json::from_str(now_resp.as_str()).unwrap();
     
     let weather_type = match now.weather.id {
         3|5   => WeatherType::DESPEJADO,
@@ -97,7 +147,7 @@ pub async fn now() -> Result<WeatherData, Box<dyn std::error::Error>> {
         89    => WeatherType::FUERTESLLUVIASTORMENTAELECTRICA,
 
         _     => WeatherType::DESPEJADO,
-    }; 
+    };
 
     
     let data = WeatherData {
@@ -110,4 +160,29 @@ pub async fn now() -> Result<WeatherData, Box<dyn std::error::Error>> {
     };
 
     Ok(data)
+}
+
+
+
+pub async fn forecast() -> Result<Vec<WeatherData>, Box<dyn std::error::Error>> {
+    
+    let headers = make_headers(get_token().await?);
+    // TODO: Save token for like, 1 min
+
+    let client = reqwest::Client::new();
+
+    let forecast_resp = client
+        .get("https://ws1.smn.gob.ar/v1/forecast/location/7706")
+        .headers(headers)
+        .send()
+        .await?
+        .text()
+        .await?;
+
+    dbg!(&forecast_resp);
+    let forecast: ForecastParser = serde_json::from_str(forecast_resp.as_str()).unwrap();
+    dbg!(forecast);
+    
+
+    Ok(vec![])
 }
--- src/retriver.rs
+++ src/retriver.rs
@@ -2,6 +2,7 @@ use crate::providers;
 
 pub async fn retrive() -> Result<(), Box<dyn std::error::Error>> {
     let smn_now = providers::smn::now().await?;
+    providers::smn::forecast().await?;
     dbg!(smn_now);
     Ok(())
 }
